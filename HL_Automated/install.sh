8e97083fa63cf7bd43171367ded35bf24a3dec22
gogogopliz <jofran.13@gmail.com>
2025-08-10 23:15:21 +0200

    Update install.sh

--- HL_Automated/install.sh
+++ HL_Automated/install.sh
@@ -5,7 +5,7 @@ echo Installing pip
 sudo apt install python3-pip -y
 
 echo Installing virtual environment and packages
-cd LiveTradingBots/code
+cd HL_Automated/code
 sudo apt-get install python3-venv
 python3 -m venv .venv
 source .venv/bin/activate
